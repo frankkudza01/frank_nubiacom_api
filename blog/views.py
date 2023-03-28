985a497c47a11ceafbde368156ed7d71d6a36914
Kudzai M <kudzaim@sonity.net>
2023-03-28 10:16:11 +0200

    Class Based Views

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,47 @@
+from blog.models import Post
 from django.shortcuts import render
 from blog.serializer import PostSerializer
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from blog.models import Post
+from django.http import Http404
 
-@api_view(['GET','POST'])
-def post_list(request):
-    if request.method=='GET':
+
+#get all posts
+class PostList(APIView):
+    def get(self,request):
         posts=Post.objects.all()
         serializer=PostSerializer(posts,many=True)
         return Response(serializer.data)
 
-    elif request.method=='POST':
+# create new post
+class PostCreate(APIView):
+    def post(self,request):
         serializer=PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
-def post_detail(request):
-    try:
-        post=Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method=='GET':
+#get detailed post
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self,request,pk):
+        post=self.get_object(pk)
         serializer=PostSerializer(post)
         return Response(serializer.data)
 
-    elif request.method=='PUT':
+# edit or delete data
+class PostEdit(APIView):
+    def put(self,request,pk):
+        post=self.get_object(pk)
         serializer=PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -38,6 +49,7 @@ def post_detail(request):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method=='DELETE':
+    def delete(self,request,pk):
+        post=self.get_object(pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
