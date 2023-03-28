84851a492665948e0c796d1a8b0de8d2a7466e06
Kudzai M <kudzaim@sonity.net>
2023-03-28 07:40:09 +0200

    Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title=models.CharField(max_length=500)
+    description=models.TextField()
+    content=models.TextField()
+    date_posted=models.DateField(default=timezone.now)
+    category=models.CharField(max_length=100, default="Education")
